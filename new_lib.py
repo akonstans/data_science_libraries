2bb0bc25e37d73866790825bc7d8d6ce57c8aff7
Andrew Konstans <andrew.konstans10@gmail.com>
2022-11-16 12:35:43 -0600

    advanced_dataframes edits

--- new_lib.py
+++ new_lib.py
@@ -1,7 +1,20 @@
 def clean(currency):
-    '''Takes a currency considered an int and turns it into a clean float
+    '''Takes a currency considered an obj or str and turns it into a clean float
         rounded to two decimal places'''
-    x = x.replace('$', '')
-    x = x.replace(',', '')
-    x = round(float(x), 2)
-    return x
+    currency = currency.replace('$', '')
+    currency = currency.replace(',', '')
+    currency = round(float(currency), 2)
+    return currency
+
+def get_db_url(db, env_file=os.path.exists('env.py')):
+    '''
+    return a formatted string containing username, password, host and database
+    for connecting to the mySQL server
+    and the database indicated
+    env_file checks to see if the env.py exists in cwd
+    '''
+    if env_file:
+        username, password, host = (env.username, env.password, env.host)
+        return f'mysql+pymysql://{username}:{password}@{host}/{db}'
+    else:
+        return 'You need a username and password'
